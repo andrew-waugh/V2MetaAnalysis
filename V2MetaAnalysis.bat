c18d3156142459ce139b29ba24e377964dd67cc7
Andrew Waugh <andrew.waugh@prov.vic.gov.au>
2020-01-03 14:51:09 +1100

    Change the BAT file to make it portable and simple

--- V2MetaAnalysis.bat
+++ V2MetaAnalysis.bat
@@ -1,7 +1,7 @@
 @echo off
-set code="C:\Users\Andrew\Documents\Work\VERSCode\V2MetaAnalysis"
-set bin="C:\Program Files\Java\jdk1.8.0_162\bin"
-rem set code="J:\PROV\TECHNOLOGY MANAGEMENT\Application Development\VERS\VERSCode\V2MetaAnalysis"
-rem set bin="C:\Program Files\Java\jdk1.8.0_144\bin"
-set versclasspath=%code%/dist/*
-%bin%\java -classpath %versclasspath% V2MetaAnalysis.V2MetaAnalysis %*
+if exist "J:/PROV/TECHNOLOGY MANAGEMENT/Application Development/VERS/VERSCode" (
+	set code="J:/PROV/TECHNOLOGY MANAGEMENT/Application Development/VERS/VERSCode"
+) else (
+	set code="C:/Users/Andrew/Documents/Work/VERSCode"
+)
+java -classpath %code%/V2MetaAnalysis/dist/* V2MetaAnalysis.V2MetaAnalysis %*
